29e5dd0a9fc6bf1ef1cd7d990ed69d7ad6ea9443
Mariusz Strzelecki <szczeles@gmail.com>
2022-10-26 16:09:58 +0200

    Use tolerations instead of node selectors in GKE starter

--- getindata_kedro_starters/pyspark-iris-running-on-gke/{{ cookiecutter.repo_name }}/infrastructure/main.tf
+++ getindata_kedro_starters/pyspark-iris-running-on-gke/{{ cookiecutter.repo_name }}/infrastructure/main.tf
@@ -13,9 +13,9 @@ module "network" {
 
   subnets = [
     {
-      subnet_name   = var.subnetwork
-      subnet_ip     = "10.0.0.0/17"
-      subnet_region = var.region
+      subnet_name           = var.subnetwork
+      subnet_ip             = "10.0.0.0/17"
+      subnet_region         = var.region
       subnet_private_access = "true"
     },
   ]
@@ -34,8 +34,6 @@ module "network" {
   }
 }
 
-
-
 module "gke" {
   source     = "terraform-google-modules/kubernetes-engine/google"
   name       = var.cluster_name
@@ -44,15 +42,24 @@ module "gke" {
   region     = var.region
   zones      = var.zones
 
-  network                = var.network
-  subnetwork             = var.subnetwork
-  ip_range_pods          = module.network.subnets_secondary_ranges[0][0].range_name
-  ip_range_services      = module.network.subnets_secondary_ranges[0][1].range_name
-  create_service_account = true
-  grant_registry_access  = true
-  initial_node_count     = 1
+  network                  = var.network
+  subnetwork               = var.subnetwork
+  ip_range_pods            = module.network.subnets_secondary_ranges[0][0].range_name
+  ip_range_services        = module.network.subnets_secondary_ranges[0][1].range_name
+  create_service_account   = true
+  grant_registry_access    = true
+  initial_node_count       = 1
+  remove_default_node_pool = true
+
 
   node_pools = [
+    {
+      name               = "default"
+      machine_type       = "n1-standard-1"
+      autoscaling        = false
+      initial_node_count = 1
+      disk_type          = "pd-standard"
+    },
     {
       name         = "spark-drivers"
       machine_type = "n1-standard-1"
@@ -71,4 +78,44 @@ module "gke" {
       spot         = true
     },
   ]
+
+  node_pools_taints = {
+    default = [
+      {
+        key    = "components.gke.io/gke-managed-components"
+        value  = true
+        effect = "NO_SCHEDULE"
+      },
+    ]
+    spark-drivers = [
+      {
+        key    = "spark"
+        value  = "drivers"
+        effect = "NO_SCHEDULE"
+      },
+    ]
+    spark-executors = [
+      {
+        key    = "spark"
+        value  = "executors"
+        effect = "NO_SCHEDULE"
+      },
+    ]
+  }
+}
+
+module "gke_auth" {
+  source = "terraform-google-modules/kubernetes-engine/google//modules/auth"
+
+  project_id   = var.project_id
+  cluster_name = var.cluster_name
+  location     = module.gke.location
+
+  depends_on = [module.gke]
+}
+
+resource "local_file" "kubeconfig" {
+  content         = module.gke_auth.kubeconfig_raw
+  filename        = "${path.module}/.kubeconfig"
+  file_permission = 0600
 }
